f060682d81644458a7b0c9c81f3a2f1e53c5863d
Realetive <realetive@yandex.ru>
2020-03-13 06:55:43 +0300

    Update i18n

--- components/common.blocks/event/event.bemtree.js
+++ components/common.blocks/event/event.bemtree.js
@@ -1,73 +1,35 @@
-/**
- * @typedef {Object} Image
- */
-
-/**
- * @typedef {Object} LocaleString
- * @property {String} ru
- * @property {String} en
- */
-
-/**
- * @typedef {Object} Slug
- * @property {String} current
- */
-
-/**
- * @typedef {Object} Event
- * @property {Image} mainImage
- * @property {Image} sourceImage
- * @property {Slug} slug
- * @property {LocaleString} introText
- * @property {LocaleString} title
- * @property {LocaleString} content
- * @property {String} publishedAt
- * @property {String} source
- */
-
-/**
- * @typedef {Object} node
- * @property {Object} ctx
- * @function _urlFor
- * @function _fromMarkdown
- */
-
-block( 'event' )( {
-  /**
-   * @description Event
-   * @param {Node} node
-   * @param {Event} { event } Event
-   *
-   * @returns {Node} node
-   */
-  extend: (
-    {
-      _fromMarkdown,
-      _urlFor,
-      config: {
-        langs: [ i18n ],
+block( 'event' )(
+  extend()(
+    (
+      {
+        _fromMarkdown,
+        _urlFor,
+        config: {
+          langs: [ i18n ],
+        },
       },
-    },
-    { event }
-  ) => ( {
-    'ctx.event': event
-      ? {
-        image: _urlFor( event.mainImage )
-          .height( 530 )
-          .maxHeight( 530 )
-          .url(),
-        date: event.publishedAt || new Date().toISOString(),
-        title: ( event.title || {} )[ i18n ] || '',
-        description: ( event.introText || {} )[ i18n ] || '',
-        url: event.source || ( event.slug || {} ).current || '',
-        content: _fromMarkdown( ( event.content || {} )[ i18n ] || '' ),
-        source: event.source,
-        sourceImage: _urlFor( event.sourceImage )
-          .height( 35 )
-          .maxHeight( 35 )
-          .url(),
-        gallery: event.gallery,
-      }
-      : {},
-  } ),
-} );
+      { event }
+    ) => ( {
+      'ctx.event': event
+        ? {
+          image: _urlFor( event.mainImage )
+            .height( 530 )
+            .maxHeight( 530 )
+            .url(),
+          date: event.publishedAt || new Date().toISOString(),
+          title: ( event.title || {} )[ i18n ] || '',
+          description: ( event.introText || {} )[ i18n ] || '',
+          url: event.source || ( event.slug || {} ).current || '',
+          content: _fromMarkdown( ( event.content || {} )[ i18n ] || '' ),
+          source: event.source,
+          sourceImage: _urlFor( event.sourceImage )
+            .height( 35 )
+            .maxHeight( 35 )
+            .url(),
+          gallery: event.gallery,
+        }
+        : {},
+    } )
+  ),
+  match( ( node, { event } ) => !event.title ).def()( '' )
+);
--- components/common.blocks/page/_route/page_route_policy.bemtree.js
+++ components/common.blocks/page/_route/page_route_policy.bemtree.js
@@ -1,5 +1,86 @@
+const policy = {
+  heading: {
+    en: 'Personal Data Processing Consent',
+    ru: 'Согласие на обработку персональных данных',
+  },
+  user: {
+    en: 'Being the Site User ',
+    ru: 'Я, Пользователь Сайта ',
+  },
+  consent: {
+    en:
+      ', I give my consent to the processing of my personal data freely, by my own will and in my interest. This Consent has been given to the Site Administration - Northern Cable LLC. Being the Site User.',
+    ru:
+      ', даю согласие на обработку своих персональных данных свободно, своей волей и в своем интересе. Настоящее Согласие дано Администрации сайта — Обществу с ограниченной ответственностью «Северный кабель».',
+  },
+  receive: {
+    en:
+      'I agree to receive information by e-mail or phone from the Site Administration.',
+    ru:
+      'Я, Пользователь Сайта, выражаю согласие на получение информации по электронной почте или телефону от Администрации Сайта.',
+  },
+  purpose: {
+    en: 'The purpose of processing personal data is:',
+    ru: 'Целью обработки персональных данных являются:',
+  },
+  purposes: {
+    en: [
+      'being counselting,',
+      'sending information by e-mail or SMS about services and / or events,',
+      'receiving information about marketing events.',
+    ],
+    ru: [
+      'консультирование,',
+      'направление информации по электронной почте или СМС об услугах и / или мероприятиях,',
+      'получение информации о маркетинговых событиях.',
+    ],
+  },
+  scope: {
+    en:
+      'Scope of personal data processing: name/alias, phone number, email address.',
+    ru:
+      'Объем обработки персональных данных: имя/псевдоним, телефон, адрес электронной почты.',
+  },
+  actions: {
+    en:
+      'This consent is granted by me to perform actions with respect to my personal data that are necessary to achieve the above goals, including (without limitation) collection, systematization, accumulation, storage, clarification (update, change), use, depersonalization, blocking of personal data, as well as any other actions provided for by the current legislation of the Russian Federation.',
+    ru:
+      'Настоящее согласие предоставляется мной на осуществление действий в отношении моих персональных данных, которые необходимы для достижения указанных выше целей, включая (без ограничения) сбор, систематизацию, накопление, хранение, уточнение (обновление, изменение), использование, обезличивание, блокирование персональных данных, а также осуществление любых иных действий, предусмотренных действующим законодательством Российской Федерации.',
+  },
+  thirdParties: {
+    en:
+      'Personal data is not transferred to third parties, except in cases stipulated by the current legislation of the Russian Federation.',
+    ru:
+      'Персональные данные не передаются третьим лицам, за исключением случаев, предусмотренных действующим законодательством Российской Федерации.',
+  },
+  period: {
+    en:
+      'This consent to the processing of personal data is valid for an indefinite period.',
+    ru:
+      'Настоящее согласие на обработку персональных данных действует в течение неопределенного срока.',
+  },
+  revoke: {
+    en:
+      'This consent to the processing of personal data may be revoked by me by sending a written request to the site administration.',
+    ru:
+      'Настоящее согласие на обработку персональных данных может быть отозвано мною путем направления письменного обращения администрации сайта.',
+  },
+  publicLink: {
+    en: 'The personal data processing policy is available in the public domain at the link ',
+    ru: 'Политика обработки персональных данных размещена в открытом публичном доступе по ссылке ',
+  },
+  store: {
+    en: 'Personal data of Users is stored exclusively on electronic media and processed using automated systems, except for cases when non-automated processing of personal data is necessary in connection with the implementation of legal requirements.',
+    ru: 'Персональные данные Пользователей хранятся исключительно на электронных носителях и обрабатываются с использованием автоматизированных систем, за исключением случаев, когда неавтоматизированная обработка персональных данных необходима в связи с исполнением требований законодательства.',
+  },
+};
+
 block( 'page' ).mod( 'route', 'policy' )( {
-  route: [
+  route: ( {
+    config: {
+      langs: [ i18n ],
+    },
+  } ) => [
     { block: 'header' },
     {
       elem: 'layout',
@@ -11,40 +92,35 @@ block( 'page' ).mod( 'route', 'policy' )( {
         {
           block: 'heading',
           mods: { size: 'xxl' },
-          content: 'Согласие на обработку персональных данных',
+          content: policy.heading[ i18n ],
         },
         { tag: 'br' },
         {
           block: 'paragraph',
           mods: { size: 'l' },
           content: [
-            'Я, Пользователь Сайта ',
+            policy.user[ i18n ],
             {
               block: 'link',
               to: 'index',
               content: 'https://severkabel.ru/',
             },
-            ', даю согласие на обработку своих персональных данных свободно, своей волей и в своем интересе. Настоящее Согласие дано Администрации сайта — Обществу с ограниченной ответственностью «Северный кабель».',
+            policy.consent[ i18n ],
           ],
         },
         {
           block: 'paragraph',
           mods: { size: 'l' },
-          content:
-            'Я, Пользователь Сайта, выражаю согласие на получение информации по электронной почте или телефону от Администрации Сайта.',
+          content: policy.receive[ i18n ],
         },
         {
           block: 'paragraph',
           mods: { size: 'l' },
-          content: 'Целью обработки персональных данных являются:',
+          content: policy.purpose[ i18n ],
         },
         {
           block: 'list',
-          items: [
-            'консультирование,',
-            'направление информации по электронной почте или СМС об услугах и / или мероприятиях,',
-            'получение информации о маркетинговых событиях.',
-          ],
+          items: policy.purposes[ i18n ],
         },
         {
           tag: 'br',
@@ -52,50 +128,45 @@ block( 'page' ).mod( 'route', 'policy' )( {
         {
           block: 'paragraph',
           mods: { size: 'l' },
-          content:
-            'Объем обработки персональных данных: имя/псевдоним, телефон, адрес электронной почты.',
+          content: policy.scope[ i18n ],
         },
         {
           block: 'paragraph',
           mods: { size: 'l' },
-          content:
-            'Настоящее согласие предоставляется мной на осуществление действий в отношении моих персональных данных, которые необходимы для достижения указанных выше целей, включая (без ограничения) сбор, систематизацию, накопление, хранение, уточнение (обновление, изменение), использование, обезличивание, блокирование персональных данных, а также осуществление любых иных действий, предусмотренных действующим законодательством Российской Федерации.',
+          content: policy.actions[ i18n ],
         },
         {
           block: 'paragraph',
           mods: { size: 'l' },
-          content:
-            'Персональные данные не передаются третьим лицам, за исключением случаев, предусмотренных действующим законодательством Российской Федерации.',
+          content: policy.thirdParties[ i18n ],
         },
         {
           block: 'paragraph',
           mods: { size: 'l' },
-          content:
-            'Настоящее согласие на обработку персональных данных действует в течение неопределенного срока.',
+          content: policy.period[ i18n ],
         },
         {
           block: 'paragraph',
           mods: { size: 'l' },
-          content:
-            'Настоящее согласие на обработку персональных данных может быть отозвано мною путем направления письменного обращения администрации сайта.',
+          content: policy.revoke[ i18n ],
         },
         {
           block: 'paragraph',
           mods: { size: 'l' },
           content: [
-            'Политика обработки персональных данных размещена в открытом публичном доступе по ссылке ',
+            policy.publicLink[ i18n ],
             {
               block: 'link',
               to: 'policy',
               content: 'https://severkabel.ru/policy',
             },
+            '.',
           ],
         },
         {
           block: 'paragraph',
           mods: { size: 'l' },
-          content:
-            'Персональные данные Пользователей хранятся исключительно на электронных носителях и обрабатываются с использованием автоматизированных систем, за исключением случаев, когда неавтоматизированная обработка персональных данных необходима в связи с исполнением требований законодательства.',
+          content: policy.store[ i18n ],
         },
       ],
     },
